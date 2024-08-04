1f44f23730a69b16ed21e300f5069cd479162207
Thato Monyamane <62892510+thato2-5@users.noreply.github.com>
2024-08-04 20:42:18 +0200

    Add files via upload

---
+++ Blog/index01.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // Hide loading overlay after page load
+    $(window).on('load', function() {
+        $('#loading-overlay').fadeOut();
+    });
+    
+    // Handle click events
+    $('#subscribeBtn').click(function() {
+        alert('Subscribe button clicked!');
+    });
+    
+    $('#signInBtn').click(function() {
+        alert('Sign In button clicked!');
+    });
+});
